b468d2395ac2a5a202032ea14932a759b5ea9683
mckaygerhard <mckaygerhard@gmail.com>
2023-06-07 17:24:12 -0400

    farming - make apples 3d backporting the 3d_apples mod

--- mods/farming/compatibility.lua
+++ mods/farming/compatibility.lua
@@ -8,9 +8,34 @@ local alias = function(orig, new)
 	minetest.register_alias(orig, new)
 end
 
+local boxapple = {
+	type = "fixed",
+	fixed = {
+			{-0.1875, -0.4375, -0.1875, 0.1875, 0.0625, 0.1875}, -- Bottom
+			{-0.25, -0.375, -0.25, 0.25, 0, 0.25}, -- Middle
+			-- {-0.1875, 0.0625, -0.1875, 0.1875, 0, 0.1875}, -- Top
+			{-0.0625, 0.0625, -0.0625, 0, 0.125, 0}, -- NodeBox4
+			{0.0625, 0.0625, 0.0625, 0, 0.125, 0}, -- NodeBox5
+			{0.0625, 0.125, -0.0625, 0, 0.1875, 0}, -- NodeBox4
+			{-0.0625, 0.125, 0.0625, 0, 0.1875, 0}, -- NodeBox5
+		}
+	}
+
 --= Overrides (add food_* group to apple and brown mushroom)
 
 minetest.override_item("default:apple", {
+	drawtype = "nodebox",
+	paramtype = "light",
+	node_box = boxapple,
+	selection_box = boxapple,
+	tiles = { -- +Y, -Y, +X, -X, +Z, -Z
+		"apple_bottom.png^apple_top_overlay.png",
+		"apple_bottom.png",
+		"apple_side.png",
+		"apple_side.png",
+		"apple_side.png",
+		"apple_side.png",
+	},
 	groups = {food_apple = 1, fleshy = 3, dig_immediate = 3, flammable = 2,
 		leafdecay = 3, leafdecay_drop = 1}
 })
